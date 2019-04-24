81fd9062562da7cdab9c142d8ff6d7b5566268b2
SAMURAIRYU <49862731+SAMURAIRYU@users.noreply.github.com>
2019-04-24 01:16:20 -0700

    Create Indeed.m

---
+++ Indeed.m
@@ -0,0 +1,20 @@
+Index of /public/Operating Systems/Microsoft Official/
+../
+Microsoft DOS/                                     03-Nov-2017 18:24       -
+Microsoft Windows 1.x/                             03-Nov-2017 18:24       -
+Microsoft Windows 2.x/                             03-Nov-2017 18:24       -
+Microsoft Windows 2000/                            03-Nov-2017 18:24       -
+Microsoft Windows 3.0/                             03-Nov-2017 18:24       -
+Microsoft Windows 3.1/                             03-Nov-2017 18:24       -
+Microsoft Windows 3.11/                            03-Nov-2017 18:24       -
+Microsoft Windows 3.2/                             03-Nov-2017 18:24       -
+Microsoft Windows 95/                              03-Nov-2017 18:24       -
+Microsoft Windows 98/                              03-Nov-2017 18:24       -
+Microsoft Windows ME/                              03-Nov-2017 18:24       -
+Microsoft Windows for Workgroups 3.1/              03-Nov-2017 18:25       -
+Microsoft Windows for Workgroups 3.11/             03-Nov-2017 18:25       -
+Looking for newer versions?.txt                    25-Apr-2018 08:18     555
+ 
+wget -m -np -c -w 3 --no-check-certificate -R "index.html*" "https://the-eye.eu/public/Operating%20Systems/Microsoft%20Official/"
+ Copy
+dmca
